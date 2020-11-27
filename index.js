8b165fbd3cf63b06f5a6f7f31ac314e3a1f0ce47
Jason <jasontsemf@gmail.com>
2020-11-27 18:15:51 -0500

    dumb port no

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 let app = express();
 const PORT = process.env.PORT || 8080;
-const server = app.listen(3000);
+const server = app.listen(PORT);
 // let socket = require('socket.io');
 // let io = socket(server);
 const io = require('socket.io')(server, {
